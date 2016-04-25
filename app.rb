e5a54f3c1dd8911e6fc03569c1427c862e917fb6
Mara Wanot <m.d.wanot@hotmail.com>
2016-04-25 17:51:36 +0100

    My name is cat

--- app.rb
+++ app.rb
@@ -1,15 +1,5 @@
 require 'sinatra'
 
-get '/' do
-  "Silver Onions"
-end
-
-get '/secret' do
-  "Bad Onions"
-end
-
 get '/cat' do
-  "<div style='border: 5px dashed red'>
-  <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
